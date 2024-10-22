17f39d380902ee6f657002e50e5bf32449dc85d7
nghia bui <nghia.bui@apac.merkleinc.com>
2024-10-22 14:37:04 +0700

    remove unnecessary

--- src/utility/AssertUtility.ts
+++ src/utility/AssertUtility.ts
@@ -1,15 +1,21 @@
 import { test, expect } from "@playwright/test";
 
 export default class AssertUtility {
-  static async assertEqual(actual: string, expected: string, message?: string) {
-    await test.step(`Assert equal actual: ${actual} expected: ${expected}`, async () => {
-      expect.soft(actual, message).toEqual(expected);
+  static async assertEqual(
+    actual: string,
+    expected: string,
+    description?: string
+  ) {
+    // eslint-disable-next-line playwright/no-conditional-in-test
+    await test.step(`Assert equal actual: ${actual} expected: ${expected} ${description ? " | Description: " + description : ""}`, async () => {
+      expect.soft(actual).toEqual(expected);
     });
   }
 
-  static async assertTrue(condition: boolean, message?: string) {
-    await test.step(`Assert true actual: ${condition}`, async () => {
-      expect.soft(condition, message).toBeTruthy();
+  static async assertTrue(condition: boolean, description?: string) {
+    // eslint-disable-next-line playwright/no-conditional-in-test
+    await test.step(`Assert true actual: ${condition} ${description ? " | Description: " + description : ""}`, async () => {
+      expect.soft(condition).toBeTruthy();
     });
   }
 }
