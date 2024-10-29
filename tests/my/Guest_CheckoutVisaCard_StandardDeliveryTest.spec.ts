0ec660699c5e4a6b4df5911c9b42403a8ba266d0
nghia bui <nghia.bui@apac.merkleinc.com>
2024-10-29 14:41:55 +0700

    divine test into steps

--- tests/my/Guest_CheckoutVisaCard_StandardDeliveryTest.spec.ts
+++ tests/my/Guest_CheckoutVisaCard_StandardDeliveryTest.spec.ts
@@ -16,53 +16,60 @@ test("Guest_CheckoutVisaCard_StandardDeliveryTestSpec", async ({
   visaCard,
   calculated,
 }) => {
-  await homePage.search(variantProduct1.getName());
-  await productListPage.viewProductDetail(variantProduct1);
-  await productDetailPage.setSize(variantProduct1.getSize());
-  variantProduct1.setPrice(await productDetailPage.getPrice());
-  await productDetailPage.addToCart(variantProduct1);
-
-  await homePage.viewCart();
-  await shoppingCartPage.checkout();
-
-  await checkoutPage.fillInShippingInformation(
-    randomAddress,
-    randomCustomerInformation
-  );
-  await checkoutPage.submitShipping();
-  await checkoutPage.setShippingMethod(shippingMethodStandard);
-  await checkoutPage.isCreditCardMethodChecked();
-  await checkoutPage.submitShippingMethod();
-  await checkoutPage.setPaymentMethod(paymentMethodCreditCard);
-
   const { subTotal, shippingFee, grandTotal } = calculated(
     shippingMethodStandard,
     variantProduct1
   );
-  await checkoutPage.checkProduct(variantProduct1);
-  await checkoutPage.checkShippingFee(shippingFee);
-  await checkoutPage.checkSubtotalPrice(subTotal);
-  await checkoutPage.checkGrandTotalPrice(grandTotal);
 
-  await checkoutPage.placeOrder();
+  await test.step("Add product to cart", async () => {
+    await homePage.search(variantProduct1.getName());
+    await productListPage.viewProductDetail(variantProduct1);
+    await productDetailPage.setSize(variantProduct1.getSize());
+    variantProduct1.setPrice(await productDetailPage.getPrice());
+    await productDetailPage.addToCart(variantProduct1);
+  });
 
-  await molpayPage.fillInCard(visaCard);
-  await molpayPage.selectCountry();
-  await molpayPage.fillBankInformation("Bank Name", "Description");
-  await molpayPage.agreeTerms();
-  await molpayPage.payOnline();
-  await molpayPage.requestOTP();
-  await molpayPage.fillInOTP();
-  await molpayPage.payNow();
+  await test.step("Checkout", async () => {
+    await homePage.viewCart();
+    await shoppingCartPage.checkout();
 
-  await thankYouPage.getOrderNumber();
-  await thankYouPage.checkProduct(variantProduct1);
-  await thankYouPage.checkShippingFee(shippingFee);
-  await thankYouPage.checkSummarySubtotalPrice(subTotal);
-  await thankYouPage.checkSummaryGrandTotalPrice(grandTotal);
-  await thankYouPage.checkShippingMethod(shippingMethodStandard);
-  await thankYouPage.checkShippingAddress(randomAddress);
-  await thankYouPage.checkBillingAddress(randomAddress);
-  await thankYouPage.checkPaymentMethod(paymentMethodCreditCard);
-  await thankYouPage.checkPaymentAmount(grandTotal);
+    await checkoutPage.fillInShippingInformation(
+      randomAddress,
+      randomCustomerInformation
+    );
+    await checkoutPage.submitShipping();
+    await checkoutPage.setShippingMethod(shippingMethodStandard);
+    await checkoutPage.isCreditCardMethodChecked();
+    await checkoutPage.submitShippingMethod();
+    await checkoutPage.setPaymentMethod(paymentMethodCreditCard);
+
+    await checkoutPage.checkProduct(variantProduct1);
+    await checkoutPage.checkShippingFee(shippingFee);
+    await checkoutPage.checkSubtotalPrice(subTotal);
+    await checkoutPage.checkGrandTotalPrice(grandTotal);
+  });
+
+  await test.step("Payment", async () => {
+    await checkoutPage.placeOrder();
+
+    await molpayPage.fillInCard(visaCard);
+    await molpayPage.selectCountry();
+    await molpayPage.fillBankInformation("Bank Name", "Description");
+    await molpayPage.agreeTerms();
+    await molpayPage.payOnline();
+    await molpayPage.requestOTP();
+    await molpayPage.fillInOTP();
+    await molpayPage.payNow();
+
+    await thankYouPage.getOrderNumber();
+    await thankYouPage.checkProduct(variantProduct1);
+    await thankYouPage.checkShippingFee(shippingFee);
+    await thankYouPage.checkSummarySubtotalPrice(subTotal);
+    await thankYouPage.checkSummaryGrandTotalPrice(grandTotal);
+    await thankYouPage.checkShippingMethod(shippingMethodStandard);
+    await thankYouPage.checkShippingAddress(randomAddress);
+    await thankYouPage.checkBillingAddress(randomAddress);
+    await thankYouPage.checkPaymentMethod(paymentMethodCreditCard);
+    await thankYouPage.checkPaymentAmount(grandTotal);
+  });
 });
--- tests/sg/Guest_CheckoutVisaCard_StandardDeliveryTest.spec.ts
+++ tests/sg/Guest_CheckoutVisaCard_StandardDeliveryTest.spec.ts
@@ -16,47 +16,53 @@ test("Guest_CheckoutVisaCard_StandardDeliveryTestSpec", async ({
   visaCard,
   calculated,
 }) => {
-  await homePage.search(variantProduct1.getName());
-  await productListPage.viewProductDetail(variantProduct1);
-  await productDetailPage.setSize(variantProduct1.getSize());
-  variantProduct1.setPrice(await productDetailPage.getPrice());
-  await productDetailPage.addToCart(variantProduct1);
-
-  await homePage.viewCart();
-  await shoppingCartPage.checkout();
-
-  await checkoutPage.fillInShippingInformation(
-    randomAddress,
-    randomCustomerInformation
-  );
-  await checkoutPage.submitShipping();
-  await checkoutPage.setShippingMethod(shippingMethodStandard);
-  await checkoutPage.isCreditCardMethodChecked();
-  await checkoutPage.submitShippingMethod();
-  await checkoutPage.setPaymentMethod(paymentMethodCreditCard);
-
   const { subTotal, shippingFee, grandTotal } = calculated(
     shippingMethodStandard,
     variantProduct1
   );
-  await checkoutPage.checkProduct(variantProduct1);
-  await checkoutPage.checkShippingFee(shippingFee);
-  await checkoutPage.checkSubtotalPrice(subTotal);
-  await checkoutPage.checkGrandTotalPrice(grandTotal);
 
-  await checkoutPage.placeOrder();
+  await test.step("Add product to cart", async () => {
+    await homePage.search(variantProduct1.getName());
+    await productListPage.viewProductDetail(variantProduct1);
+    await productDetailPage.setSize(variantProduct1.getSize());
+    variantProduct1.setPrice(await productDetailPage.getPrice());
+    await productDetailPage.addToCart(variantProduct1);
+  });
 
-  await worldPayPage.fillInCard(visaCard);
-  await worldPayPage.makePayment();
-  await thankYouPage.getOrderNumber();
+  await test.step("Checkout", async () => {
+    await homePage.viewCart();
+    await shoppingCartPage.checkout();
 
-  await thankYouPage.checkProduct(variantProduct1);
-  await thankYouPage.checkShippingFee(shippingFee);
-  await thankYouPage.checkSummarySubtotalPrice(subTotal);
-  await thankYouPage.checkSummaryGrandTotalPrice(grandTotal);
-  await thankYouPage.checkShippingMethod(shippingMethodStandard);
-  await thankYouPage.checkShippingAddress(randomAddress);
-  await thankYouPage.checkBillingAddress(randomAddress);
-  await thankYouPage.checkPaymentMethod(paymentMethodCreditCard);
-  await thankYouPage.checkPaymentAmount(grandTotal);
+    await checkoutPage.fillInShippingInformation(
+      randomAddress,
+      randomCustomerInformation
+    );
+    await checkoutPage.submitShipping();
+    await checkoutPage.setShippingMethod(shippingMethodStandard);
+    await checkoutPage.isCreditCardMethodChecked();
+    await checkoutPage.submitShippingMethod();
+    await checkoutPage.setPaymentMethod(paymentMethodCreditCard);
+
+    await checkoutPage.checkProduct(variantProduct1);
+    await checkoutPage.checkShippingFee(shippingFee);
+    await checkoutPage.checkSubtotalPrice(subTotal);
+    await checkoutPage.checkGrandTotalPrice(grandTotal);
+  });
+
+  await test.step("Payment", async () => {
+    await checkoutPage.placeOrder();
+    await worldPayPage.fillInCard(visaCard);
+    await worldPayPage.makePayment();
+    await thankYouPage.getOrderNumber();
+
+    await thankYouPage.checkProduct(variantProduct1);
+    await thankYouPage.checkShippingFee(shippingFee);
+    await thankYouPage.checkSummarySubtotalPrice(subTotal);
+    await thankYouPage.checkSummaryGrandTotalPrice(grandTotal);
+    await thankYouPage.checkShippingMethod(shippingMethodStandard);
+    await thankYouPage.checkShippingAddress(randomAddress);
+    await thankYouPage.checkBillingAddress(randomAddress);
+    await thankYouPage.checkPaymentMethod(paymentMethodCreditCard);
+    await thankYouPage.checkPaymentAmount(grandTotal);
+  });
 });
